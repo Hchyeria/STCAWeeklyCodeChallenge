9eeac887255980bd1b71219a61bf66564cddd1e4
Hchyeria <t-qhuang@microsoft.com>
2020-08-06 23:20:42 +0800

    add 135 solution 2

--- week7/135.java
+++ week7/135.java
@@ -28,4 +28,37 @@ class Solution {
 
     // Time = O(n)
     // Space = O(n)
+
+    // Solution 2: one pass O(1) space
+    // https://leetcode.com/problems/candy/discuss/135698/Simple-solution-with-one-pass-using-O(1)-space
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        } 
+        int n = ratings.length;
+        int up = 0, down = 0, peek = 0;
+        int res = 1;
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                up++;
+                down = 0;
+                peek = up;
+                res += 1 + up;
+            } else if (ratings[i] == ratings[i - 1]) {
+                up = down = peek = 0;
+                res += 1;
+            } else {
+                down++;
+                up = 0;
+                res += down;
+                // lazy update peek
+                if (peek < down) {
+                    res += 1;
+                }
+            }
+        }
+        return res;
+    }
+    // Time = O(n)
+    // Space = O(1)
 }
